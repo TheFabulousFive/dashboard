d6f211c9031a33c2ed7eb901893cbbba914a1804
Crystal Contreras <crystalcontreras@Crystals-MacBook.local>
2018-05-05 15:22:36 -0700

    Updated sidebar image and working on TableList page

--- frontend/src/views/TableList/TableList.jsx
+++ frontend/src/views/TableList/TableList.jsx
@@ -10,6 +10,36 @@ class TableList extends Component {
       <div className="content">
         <Grid fluid>
           <Row>
+            <Col lg={3} sm={6}>
+              <StatsCard
+                bigIcon={<i className="pe-7s-server text-warning" />}
+                statsText="Total Unique Fans Engaged"
+                statsValue="401,0894"
+                statsIcon={<i className="fa fa-refresh" />}
+                statsIconText="Updated now"
+              />
+            </Col>
+            <Col lg={3} sm={6}>
+              <StatsCard
+                bigIcon={<i className="pe-7s-wallet text-success" />}
+                statsText="Revenue"
+                statsValue="$1,345"
+                statsIcon={<i className="fa fa-calendar-o" />}
+                statsIconText="Last day"
+              />
+            </Col>
+
+            <Col lg={3} sm={6}>
+              <StatsCard
+                bigIcon={<i className="fa fa-twitter text-info" />}
+                statsText="Followers"
+                statsValue="+45"
+                statsIcon={<i className="fa fa-refresh" />}
+                statsIconText="Updated now"
+              />
+            </Col>
+          </Row>
+          <Row>
             <Col md={12}>
               <Card
                 title="Striped Table with Hover"
