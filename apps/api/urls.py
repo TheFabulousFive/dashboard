1c32e59cc50012450fa3a0cbe364d8afda9579ad
George Marshall <george@georgemarshall.name>
2018-05-05 23:00:00 -0700

    Add stage viewsets

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from rest_framework.routers import SimpleRouter
 from rest_framework.documentation import include_docs_urls
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
-v1_router = SimpleRouter()
+v1_router = DefaultRouter()
 v1_router.register(r'attendees', views.AttendeeViewSet)
+v1_router.register(r'stages', views.StageViewSet)
 
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,15 +1,19 @@
-from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import (
     CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 )
+from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
 
-from .models import Attendee
-from .serializers import AttendeeSerializer
+from .models import Attendee, Stage
+from .serializers import AttendeeSerializer, StageSerializer
 
 
-# Create your views here.
 class AttendeeViewSet(GenericViewSet, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin,
                       DestroyModelMixin):
     queryset = Attendee.objects.all()
     serializer_class = AttendeeSerializer
     lookup_field = 'user_id'
+
+
+class StageViewSet(ReadOnlyModelViewSet):
+    queryset = Stage.objects.all()
+    serializer_class = StageSerializer
