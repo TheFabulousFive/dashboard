a3ae1a9ae6846a70891ec02f985120fe00520293
Matthew Miller <kale@iammiller.com>
2018-05-05 22:25:57 -0700

    Getting a start on playlists

--- apps/homepage/urls.py
+++ apps/homepage/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'homepage'
 urlpatterns = [
-    path('', views.homepage),
+    path('', views.homepage, name='index'),
+    path('attendee/<str:user_id>/', views.attendee_list, name='playlist')
 ]
