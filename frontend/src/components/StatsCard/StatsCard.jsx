ff21c427c70b5b31a3ded16dfedaabde57d588c6
meganmcgeee <meganpmcgee@gmail.com>
2018-05-05 22:37:57 -0700

    Changed column formating in status cards.

--- frontend/src/components/StatsCard/StatsCard.jsx
+++ frontend/src/components/StatsCard/StatsCard.jsx
@@ -1,5 +1,5 @@
+import { Col, Row } from "react-bootstrap";
 import React, { Component } from "react";
-import { Row, Col } from "react-bootstrap";
 
 export class StatsCard extends Component {
   render() {
@@ -7,15 +7,10 @@ export class StatsCard extends Component {
       <div className="card card-stats">
         <div className="content">
           <Row>
-            <Col xs={5}>
-              <div className="icon-big text-center icon-warning">
-                {this.props.bigIcon}
-              </div>
-            </Col>
-            <Col xs={7}>
+            <Col xs={12}>
               <div className="numbers">
-                <p>{this.props.statsText}</p>
                 {this.props.statsValue}
+                <p>{this.props.statsText}</p>
               </div>
             </Col>
           </Row>
